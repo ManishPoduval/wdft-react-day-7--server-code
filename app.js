33e7eba9b91ce54124b6383575cc42033ca30741
Manish Poduval <manishpoduval@hotmail.com>
2021-03-09 14:25:42 +0530

    deploy test

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ app.use(session({
 
 //---------------------------------------------------------
 //---------------------------------------------------------
-
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controled from the routes/index.js
@@ -54,6 +55,12 @@ app.use("/api", stripeRoutes);
 
 const cloudinaryRoutes = require("./routes/cloudinary.routes");
 app.use("/api", cloudinaryRoutes);
+
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
